77d1bf165e7e23177ce68aaaf82cce9e34ce3399
William Davis <william.j.davis5@gmail.com>
2019-12-02 21:41:41 -0500

    crawler kinda works now

--- src/classes/WebCrawler.ts
+++ src/classes/WebCrawler.ts
@@ -4,12 +4,19 @@ import cheerio = require('cheerio');
 export class WebCrawler implements ISiteCrawler {
 
     siteQueuer: ISiteQueuer;
+    private scannedUris: Array<string>;
     
     constructor(queuer: ISiteQueuer) {
         this.siteQueuer = queuer;
+        this.scannedUris = new Array<string>();
     }
 
     public async crawl(uri: string) {
+        if(this.scannedUris.includes(uri)){
+            console.debug("Not adding already scanned item")
+            return;
+        }
+        this.scannedUris.push(uri);
         let page: string
         console.log("Loading page: " + uri);
         await request.get(uri)
@@ -20,6 +27,10 @@ export class WebCrawler implements ISiteCrawler {
         let foundUris: Array<string> = await this.getUris(page);
         foundUris.forEach(element => {
             console.debug(element);
+            if(this.scannedUris.includes(element)){
+                console.debug("Not adding already scanned item")
+                return;
+            }
             this.siteQueuer.queue(element);
         });
     }
@@ -29,9 +40,12 @@ export class WebCrawler implements ISiteCrawler {
 
         let cheerioPage = cheerio.load(body);
         const aTags = cheerioPage('a');
-        for(let i = 0; i <aTags.length; i++){
+        for(let i = 0; i < aTags.length; i++){
             let href: string = aTags[i].attribs.href;
+
+            //ignore page anchors
             if(href.indexOf("#") != -1)continue;
+            //not handling relative uris right now
             if(href.indexOf("http") === -1)continue;
             foundUris.push(href);
         }
